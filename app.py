7c0553b15cfa92215f4ffae7cba7538adf151ab7
FraidoonOmarzai <fraidoon_omarzai@yahoo.com>
2022-06-20 10:44:57 +0530

    heart disease project added

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, request
+import joblib
+import numpy as np
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
     return render_template("main.html")
+
+
+# Heart Prediction
+@app.route("/heart")
+def heart():
+    return render_template("heart.html")
+
+
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1, size)
+    if(size == 13):
+        loaded_model = joblib.load("Trained Model/Heart-Model/heart_model.pkl")
+        result = loaded_model.predict(to_predict)
+
+    return result[0]
+
+
+@app.route('/predictHD', methods=["POST"])
+def predictHD():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        # print(to_predict_list)
+        to_predict_list = list(to_predict_list.values())
+        # print(to_predict_list)
+        to_predict_list = list(map(float, to_predict_list))
+        # print(to_predict_list)
+        if(len(to_predict_list) == 13):
+            result = ValuePredictor(to_predict_list, 13)
+
+    if(int(result) == 1):
+        prediction = "Sorry! it seems getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
 
 
 if __name__ == "__main__":
