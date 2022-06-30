f3279599620ff5357daf8a93df343f2d3576a8a2
FraidoonOmarzai <fraidoon_omarzai@yahoo.com>
2022-06-30 07:54:51 +0530

    liver disease prediction is added

--- app.py
+++ app.py
@@ -125,6 +125,30 @@ def predictKD():
         prediction = "No need to fear. You have no dangerous symptoms of the disease"
     return(render_template("result.html", prediction_text=prediction))
 
+# Liver disease
+@app.route("/liver")
+def liver():
+    return render_template("liver.html")
+
+
+@app.route('/predictLD', methods=["POST"])
+def predictLD():
+
+    loaded_model = joblib.load('Trained Model/liver/liver_model.pkl')
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+
+        to_predict = np.array(to_predict_list).reshape(1, len(to_predict_list))
+        result = loaded_model.predict(to_predict)
+
+    if(int(result) == 1):
+        prediction = "Sorry! it seems getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
